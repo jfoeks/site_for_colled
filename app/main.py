05cc70137a555b09f8c9afc47d1ca08c3e138621
jfoeks <rfta002@mail.ru>
2025-02-11 07:14:13 +0200

    124

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.routers import main_pages, api
+
 
 app = FastAPI()
 
